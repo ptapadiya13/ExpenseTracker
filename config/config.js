257e065fc7ef17108fc46c3663799fc7ed04e31a
ptapadiya13 <prachi.tapadiya18@vit.edu>
2022-03-10 20:30:43 +0530

    initial commit

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ",
+  mongoUri: process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject'
+}
+
+export default config
